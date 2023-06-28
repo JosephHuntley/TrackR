065738ee29b4638e5e9f851cc70c4f9f16c99b12
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-06-28 15:57:35 -0400

    Add Password class for Hash function

---
+++ TrackR/TrackR.DataContext.SQL/Password.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Controller
+{
+    public static class Password
+    {
+        const int keySize = 64;
+        const int iterations = 350000;
+        private static HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+
+        public static string HashPassword(string password, byte[] salt)
+        {
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+            salt,
+            iterations,
+                hashAlgorithm,
+                keySize);
+            return Convert.ToHexString(hash);
+        }
+        
+    }
+}
+
