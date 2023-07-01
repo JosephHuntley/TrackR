df1eba256e962740cf7cdfb6ddd4248a606ac8d2
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-07-01 16:22:22 -0400

    Added Further unit Test

--- TrackR/TrackR.Common.UnitTests/DbTest.cs
+++ TrackR/TrackR.Common.UnitTests/DbTest.cs
@@ -1,4 +1,5 @@
 using TrackR.DataContext.SQL;
+using TrackR.Models.SQL;
 
 namespace TrackR.Common.UnitTests;
 
@@ -11,6 +12,29 @@ public class DbTest
         {
             bool canConnect = db.Database.CanConnect(); // act
             Assert.True(canConnect); // assert
+        }
+    }
+    
+    [Fact]
+    public void ProviderIsSqlServer()
+    {
+        using (SqlContext db = new())
+        {
+            string? provider = db.Database.ProviderName;
+            Assert.Equal("Microsoft.EntityFrameworkCore.SqlServer", provider);
+        }
+    }
+
+    [Fact]
+    public void User1IsJoseph()
+    {
+        using (SqlContext db = new())
+        {
+            User user = db.Users.Single(u => u.UserId == 1);
+            Assert.Equal("Joseph",user.FirstName );
+        }
+        {
+            
         }
     }
 }
