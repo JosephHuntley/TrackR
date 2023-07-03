50cc4f7d3be069c08681f37f063e84cee2772e36
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-07-03 07:51:30 -0400

    Init index page

---
+++ TrackR/TrackR.BlazorServer/Pages/Index.razor.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+using TrackR.BlazorServer.Services;
+using TrackR.Models.SQL;
+
+namespace TrackR.BlazorServer.Pages;
+
+public partial class Index : ComponentBase
+{
+    public IEnumerable<Board> Boards { get; private set; } = null!;
+    public int ActiveBoard { get; set; } = 1;
+    [Inject] 
+    private BoardService _bs { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        // Initialize the Boards property by calling a method from the injected service
+        Boards = _bs.GetBoards();
+    }
+
+    public void HandleBoardClick(int id)
+    {
+        ActiveBoard = id;
+    }
+}
