65634fd638807bb9487cf004fe8f896bf4c34f2c
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-07-06 11:38:08 -0400

    Revised CreateBoardModal by adding seperate file for code

---
+++ TrackR/TrackR.BlazorServer/Components/CreateBoardModal.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using TrackR.BlazorServer.Services;
+using TrackR.Models.SQL;
+
+namespace TrackR.BlazorServer.Components;
+
+public partial class CreateBoardModal
+{
+    private string Name { get; set; } = null!;
+    [Inject] 
+    private BoardService _bs { get; set; } = null!;
+    [Parameter]
+    public Action ToggleModal { get; set; }
+
+    private async Task HandelCreateBoard()
+    {
+        Board b = new();
+        b.Name = Name;
+        await _bs.CreateBoardAsync(b);
+        _nm.NavigateTo(_nm.Uri, forceLoad: true);
+    }
+}
