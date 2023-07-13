fa22bb3bed6c7a0cf7f6dadc176e8fa068fdf05a
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-07-13 11:10:06 -0400

    Added create subtask functionality

--- TrackR/TrackR.BlazorServer/Components/Modals/ViewTaskDetailsModal.razor.cs
+++ TrackR/TrackR.BlazorServer/Components/Modals/ViewTaskDetailsModal.razor.cs
@@ -14,5 +14,42 @@ public partial class ViewTaskDetailsModal
     [Parameter]
     public List<Section> Sections { get; set; } = null!;
 
-    
+    private Subtask ActiveSubtask { get; set; } = null!;
+
+    private bool ToggleSubtaskModal { get; set; } = false;
+
+    private Subtask NewSt { get; set; } = new();
+
+    [Inject] 
+    private ISubtaskService _ss { get; set; } = null!;
+
+    [Inject] private ITaskService _ts { get; set; } = null!;
+
+    private bool IsStCreate { get; set; } = false;
+
+    private void ToggleStModal(Subtask? st = null)
+    {
+        if(st is not null){
+            ActiveSubtask = st;
+        }
+        ToggleSubtaskModal = !ToggleSubtaskModal;
+    }
+
+    private async void HandleSubtaskCreation()
+    {
+        // Flag for visual
+        IsStCreate = true;
+        
+        //Add the values user doesn't select
+        NewSt.Parent = ActiveTask;
+        NewSt.TaskId = ActiveTask.ActivityId;
+        NewSt.SectionId = await _ts.GetSectionIdPositionOneAsync();
+
+        _ss.CreateEntityAsync(NewSt);
+        
+        //Reset visual flag and newST
+        NewSt = new();
+        IsStCreate = false;
+        StateHasChanged();
+    }
 }
